e7a2aa834821d1c6e802bc98f70827c2b75ba7f7
Vladimir988 <optimist0706@gmail.com>
2024-03-06 17:53:41 +0200

    1.Project Setup

---
+++ src/pages/components/side-menu/side-menu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SideMenu = () => {
+    return (
+        <ul>
+            <li>Settings</li>
+            <li>Profile</li>
+            <li>Analitics</li>
+        </ul>
+    );
+};
+
+export default SideMenu;
